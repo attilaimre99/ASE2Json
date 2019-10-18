ec98465848dff1b97f314887cea098030b3d1d08
Imre Attila <incamamailbox@gmail.com>
2019-10-18 16:57:28 +0200

    Added JSON output

--- main.py
+++ main.py
@@ -27,8 +27,6 @@ class Buffer:
         self.position = 0
 
     def read(self, count):
-        # print self.position, self.position+count, ":".join("{:02x}".format(ord(c)) for c in self.text[self.position:self.position+count])
-
         ret = ""
         for i in self.text[self.position:self.position+count]:
             if ord(i) != 0:
@@ -46,8 +44,15 @@ class Buffer:
 
         for item in range(len):
             ch = self.read(1)
-            if ch == 0x22:
-                ret += "\\"
+            # print ch, chr(ch)
+            if ch == 34:
+                ret += ""
+            elif ch == 92:
+                ret += "/"
+            elif ch == 9:
+                ret += ""
+            elif ch == 10:
+                ret += ""
             else:
                 ret += chr(ch)
 
@@ -64,32 +69,40 @@ class Buffer:
             self.position = pos
 
 class Server:
-    def __init__(self, ip="", port=0, playersCount=0, maxPlayersCount=0, gameName="", serverName="", modeName="", mapName="", verName="", passworded=0, players=[], httpPort=0, serials=0):
-        self.ip = ip
-        self.port = port
-        self.playersCount = playersCount
-        self.maxPlayersCount = maxPlayersCount
-        self.gameName = gameName
-        self.serverName = serverName
-        self.modeName = modeName
-        self.mapName = mapName
-        self.verName = verName
-        self.passworded = passworded
-        self.players = players
-        self.httpPort = httpPort
-        self.serials = serials
-    
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+    def __init__(self):
+        self.ip = ""
+        self.port = 0
+        self.playersCount = 0
+        self.maxPlayersCount = 0
+        self.gameName = ""
+        self.serverName = ""
+        self.modeName = ""
+        self.mapName = ""
+        self.verName = ""
+        self.passworded = 0
+        self.players = []
+        self.httpPort = 0
+        self.serials = 0
 
-def pretty_print(clas, indent=0):
-    print(' ' * indent +  type(clas).__name__ +  ':')
-    indent += 4
-    for k,v in clas.__dict__.items():
-        if '__dict__' in dir(v):
-            pretty_print(v,indent)
-        else:
-            print(' ' * indent +  k + ': ' + str(v))
+def parse_server(buffer, servers):
+
+    count = buffer.read(4)
+
+    while buffer.step(6):
+        server = Server()
+
+        # Ip adress
+        ip_pieces = []
+        for x in range(4):
+            ip = buffer.read(1)
+            ip_pieces.append(str(ip))
+        
+        ip_pieces.reverse()
+        server.ip = '.'.join(ip_pieces)
+
+        server.port = buffer.read(2)
+
+        servers.append(server)
 
 def parse_server_v2(buffer, servers):
     flags = buffer.read(4)
@@ -105,92 +118,90 @@ def parse_server_v2(buffer, servers):
         len = buffer.read(2)
 
         # Ip adress
-        ip1 = buffer.read(1)
-        ip2 = buffer.read(1)
-        ip3 = buffer.read(1)
-        ip4 = buffer.read(1)
-        server.ip = '.'.join((str(ip4), str(ip3), str(ip2), str(ip1)))
+        ip_pieces = []
+        for x in range(4):
+            ip = buffer.read(1)
+            ip_pieces.append(str(ip))
+        
+        ip_pieces.reverse()
+        server.ip = '.'.join(ip_pieces)
 
         server.port = buffer.read(2)
  
         if (flags & Flags["ASE_PLAYER_COUNT"]) != 0:
-            server.playersCount = buffer.read(2);
+            server.playersCount = buffer.read(2)
 
         if (flags & Flags["ASE_MAX_PLAYER_COUNT"]) != 0:
-            server.maxPlayersCount = buffer.read(2);
+            server.maxPlayersCount = buffer.read(2)
 
         if (flags & Flags["ASE_GAME_NAME"]) != 0:
-            server.gameName = buffer.readString();
+            server.gameName = buffer.readString()
 
         if (flags & Flags["ASE_SERVER_NAME"]) != 0:
-            server.serverName = buffer.readString();
+            server.serverName = buffer.readString()
 
         if (flags & Flags["ASE_GAME_MODE"]) != 0:
-            server.modeName = buffer.readString();
+            server.modeName = buffer.readString()
 
         if (flags & Flags["ASE_MAP_NAME"]) != 0:
-            server.mapName = buffer.readString();
+            server.mapName = buffer.readString()
 
         if (flags & Flags["ASE_SERVER_VER"]) != 0:
-            server.verName = buffer.readString();
+            server.verName = buffer.readString()
             
         if (flags & Flags["ASE_PASSWORDED"]) != 0:
-            server.passworded = buffer.read(1);
+            server.passworded = buffer.read(1)
 
         if (flags & Flags["ASE_SERIALS"]) != 0:
-            server.serials = buffer.read(1);
+            server.serials = buffer.read(1)
 
         if (flags & Flags["ASE_PLAYER_LIST"]) != 0:
-            listSize = buffer.read(2);
+            listSize = buffer.read(2)
 
             for i in range(listSize):
                 playerNick = buffer.readString()
                 server.players.append(playerNick)
         
         # Only used for MTA, we don't care
-        noResponse = 0;
+        noResponse = 0
         if (flags & Flags["ASE_RESPONDING"]) != 0:
-            noResponse = buffer.read(1);
+            noResponse = buffer.read(1)
 
         # Only used for MTA, we don't care
-        restriction = 0;
+        restriction = 0
         if (flags & Flags["ASE_RESTRICTION"]) != 0:
-            restriction = buffer.read(4);
+            restriction = buffer.read(4)
         
         # Only used for MTA, we don't care
         if (flags & Flags["ASE_SEARCH_IGNORE_SECTIONS"]) != 0:
-            numItems = buffer.read(1);
+            numItems = buffer.read(1)
 
             # Skip
             buffer.seek(buffer.tell() + 2*numItems)
 
         # Only used for MTA, we don't care
         if (flags & Flags["ASE_KEEP_FLAG"]) != 0:
-            keepFlag = buffer.read(1);
+            keepFlag = buffer.read(1)
 
         if (flags & Flags["ASE_HTTP_PORT"]) != 0:
-            server.httpPort = buffer.read(2);
+            server.httpPort = buffer.read(2)
 
-        specialFlags = 0;
+        specialFlags = 0
         if (flags & Flags["ASE_SPECIAL"]) != 0:
-            specialFlags = buffer.read(1);
-
-        if startPos + len - buffer.tell() > 0:
-            print startPos + len - buffer.tell()
+            specialFlags = buffer.read(1)
 
         buffer.seek(startPos + len)
 
         servers.append(server)
-        
 
 def main():    
     # We get the data from the official server
     r = requests.get('https://master.multitheftauto.com/ase/mta/').content
     buffer = Buffer(r)
 
-    # Initialize server buffer
+    # Initialize servers set
     servers = []
-        
+    
     count = buffer.read(2)
     ver = 0
     if count == 0:
@@ -201,11 +212,46 @@ def main():
     if ver == 2:
         parse_server_v2(buffer, servers)
 
-    def sortFunction(item):
-        return item.playersCount
+    string = "[\n"
 
-    for item in servers:
-        print json.dumps(item)
+    firstServer = True
+
+    for server in servers:
+        if firstServer == False:
+            string += ",\n"
+
+        string += "        { "
+        string += "\"ip\": \"" + server.ip + "\", "
+        string += "\"port\": " + str(server.port) + ", "
+        string += "\"playersCount\": " + str(server.playersCount) + ", "
+        string += "\"maxPlayersCount\": " + str(server.maxPlayersCount) + ", "
+        string += "\"gameName\": \"" + server.gameName + "\", "
+        string += "\"serverName\": \"" + server.serverName + "\", "
+        string += "\"modeName\": \"" + server.modeName + "\", "
+        string += "\"mapName\": \"" + server.mapName + "\", "
+        string += "\"version\": \"" + server.verName + "\", "
+
+        if server.passworded != 0:
+            string += "\"passworded\": true, "
+        else:
+            string += "\"passworded\": false, "
+
+        string += "\"players\": ["
+
+        for playerName in server.players:
+            string += "\"" + playerName + "\", "
+
+        string += "], "
+        string += "\"httpPort\": " + str(server.httpPort)
+
+        firstServer = False
+
+        string += " }"
+
+    string += "\n    ]"
+
+
+    print(string)
 
 if __name__ == "__main__":
     main()
